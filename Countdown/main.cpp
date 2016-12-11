2e24cfcd01015dde41921cd07f5b64d9ee973cd3
MaximilianJugendForscht <wittmaxi@outlook.de>
2016-12-11 08:51:57 +0100

    Update main.cpp
    Commented everything and added a Progress bar on loading (@josh could you please explain a few things to me, marked them down in the comments)

--- Countdown/main.cpp
+++ Countdown/main.cpp
@@ -15,7 +15,7 @@
     along with Countdown.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <fstream>
+#include <fstream> //Imports all dependencies
 #include <iostream>
 #include <cassert>
 #include <string>
@@ -25,7 +25,7 @@
 #include <thread>
 #include <future>
 
-template <typename IterLhs, typename IterRhs>
+template <typename IterLhs, typename IterRhs> //@josh  could you please comment, what these variables do? (entire block)
 auto findUnionOf(IterLhs lhsIter, const IterLhs& lhsEnd,
                  const IterRhs rhsBegin, const IterRhs& rhsEnd) {
     std::vector<std::string> result;
@@ -36,46 +36,57 @@ auto findUnionOf(IterLhs lhsIter, const IterLhs& lhsEnd,
         }
     }
 
-    return result;
+    return result; //returns ther result of the for-loop
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) { //@josh what are the arguements supposed to be passed?
     // program name and word list
     if(argc < 3) {
-        std::cout << "Please supply a word to check anagrams of and a newline delimited list of words as a file" << std::endl;
-        std::exit(EXIT_FAILURE);
+        std::cout << "Please supply a word to check anagrams of and a newline delimited list of words as a file" << std::endl; //On Illegal string (too short)
+        std::exit(EXIT_FAILURE); //If the string is too short, exit the program
     }
 
-    const char* const wordlistPath = argv[2];
+    const char* const wordlistPath = argv[2]; //Sets the path of the wordlist (To have words as comparison, maybe?)
 
     std::ifstream wordlistFile(wordlistPath, std::ios_base::in);
 
     if(! wordlistFile.is_open()) {
-        std::cout << "Could not open file " << wordlistPath << std::endl;
-        std::exit(EXIT_FAILURE);
+        std::cout << "Could not open file " << wordlistPath << std::endl; //If the File with the words is empty
+        std::exit(EXIT_FAILURE); //Exits on exception FILEEMPTY
     }
 
-    std::string currentWord;
-    std::vector<std::string> words;
+    std::string currentWord; //creates a string variable with the current word for easier comparison
+    std::vector<std::string> words; //array with ALL the words inside the wordlist
 
     std::cout << "Loading words..." << std::endl;
 
+    int progressBarCounter = 0;
+    
     while(wordlistFile >> currentWord) {
         words.emplace_back(std::move(currentWord));
+        std::cout << "#"; //writes the character as Progress-bar
+        if (progressBarCounter == 30) {
+         
+            for (int i = 0, i = 30, i++) { //If the Progress bar is at thirty, flush everything
+                std::cout << "\b";
+            }
+        }
     }
 
-    std::cout << words.size() << " words loaded!" << std::endl;
+    
+    
+    std::cout << words.size() << " words loaded!" << std::endl; //tells the user how many words got loaded
 
-    std::string anagram = argv[1];
+    std::string anagram = argv[1]; //creates an instance of string called anagram, filled with the passed arg for anagramsearch
 
-    std::cout << "Searching for anagrams of " << anagram << std::endl;
+    std::cout << "Searching for anagrams of " << anagram << std::endl; //message
 
-    std::sort(anagram.begin(), anagram.end());
+    std::sort(anagram.begin(), anagram.end()); //@josh Idk this method, could you please explain it to me?
 
     std::vector<std::string> anagramPermutations;
 
     do {
-        anagramPermutations.push_back(anagram);
+        anagramPermutations.push_back(anagram); //anagramPermutations: symbol not defined
     } while(std::next_permutation(anagram.begin(), anagram.end()));
 
     std::cout << "There are " << anagramPermutations.size() << " possible words" << std::endl;
